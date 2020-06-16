cd10ec89bc348b39868d75dacaa74452d76326f9
Sean <seandatasci@gmail.com>
2020-06-15 18:31:38 -0700

    Update unet_baseline.py

--- model/unet_baseline.py
+++ model/unet_baseline.py
@@ -71,7 +71,7 @@ def decoder_block(inputs, filters, kernel_size, transpose=True):
 # INTENDED API
 # ----------------------------------------------------------------------------
 
-def unet(input_size=(256, 256, 1), output_channels=1, transpose=True):
+def unet(input_size=(800, 600, 1), output_channels=1, transpose=True):
     """
     U-net implementation adapted translated from authors original
     source code available here: 
